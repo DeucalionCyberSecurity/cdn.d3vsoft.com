97443d70298a953e3386fe9e6c483ce1272584a8
DeucalionCyberSecurity <admin@d3vsoft.com>
2024-10-15 02:25:55 +0300

    Update expozy.js

--- expozy.js
+++ expozy.js
@@ -1,50 +1,49 @@
 class ExpozyElement extends HTMLElement {
     constructor() {
-      super();
-      const shadow = this.attachShadow({ mode: 'closed' });
-  
-      // Базов URL за зареждане на файловете
-      const baseUrl = "https://cdn.d3vsoft.com";
-  
-      // Извличане на параметъра 'p' само от URL на скрипта
-      const scriptSrc = document.currentScript.src;
-      const urlParams = new URL(scriptSrc).searchParams;
-      const projectName = urlParams.get('p');
-  
-      if (projectName) {
-        // Зарежда js.json и css.json от съответната папка
-        Promise.all([
-          fetch(`${baseUrl}/${projectName}/js/import.json`).then(response => response.json()),
-          fetch(`${baseUrl}/${projectName}/css/import.json`).then(response => response.json())
-        ]).then(([jsFiles, cssFiles]) => {
-          const wrapper = document.createElement('div');
-          
-          // Вгражда CSS файловете от css.json
-          cssFiles.forEach(cssFile => {
-            const link = document.createElement('link');
-            link.rel = 'stylesheet';
-            link.href = `${baseUrl}/${cssFile}`;
-            wrapper.appendChild(link);
-          });
-  
-          // Вгражда JS файловете от js.json
-          jsFiles.forEach(jsFile => {
-            const script = document.createElement('script');
-            script.src = `${baseUrl}/${jsFile}`;
-            script.defer = true;
-            wrapper.appendChild(script);
-          });
-  
-          shadow.appendChild(wrapper);
-        }).catch(error => {
-          console.error('Грешка при зареждане на JSON файловете:', error);
-        });
-      } else {
-        console.warn("Не е зададен проектен параметър 'p' в URL адреса на скрипта.");
-      }
+        super();
+        const shadow = this.attachShadow({ mode: 'closed' });
+
+        // Динамично извличане на базовия URL на сайта
+        const baseUrl = window.location.origin;
+
+        // Извличане на параметъра 'p' само от URL на скрипта
+        const scriptSrc = document.currentScript.src;
+        const urlParams = new URL(scriptSrc).searchParams;
+        const projectName = urlParams.get('p');
+
+        if (projectName) {
+            // Зарежда js.json и css.json от съответната папка
+            Promise.all([
+                fetch(`${baseUrl}/${projectName}/js.json`).then(response => response.json()),
+                fetch(`${baseUrl}/${projectName}/css.json`).then(response => response.json())
+            ]).then(([jsFiles, cssFiles]) => {
+                const wrapper = document.createElement('div');
+
+                // Вгражда CSS файловете от css.json
+                cssFiles.forEach(cssFile => {
+                    const link = document.createElement('link');
+                    link.rel = 'stylesheet';
+                    link.href = `${baseUrl}/${cssFile}`;
+                    wrapper.appendChild(link);
+                });
+
+                // Вгражда JS файловете от js.json
+                jsFiles.forEach(jsFile => {
+                    const script = document.createElement('script');
+                    script.src = `${baseUrl}/${jsFile}`;
+                    script.defer = true;
+                    wrapper.appendChild(script);
+                });
+
+                shadow.appendChild(wrapper);
+            }).catch(error => {
+                console.error('Грешка при зареждане на JSON файловете:', error);
+            });
+        } else {
+            console.warn("Не е зададен проектен параметър 'p' в URL адреса на скрипта.");
+        }
     }
-  }
-  
-  // Регистриране на Web Component-а като expozy-element
-  customElements.define('expozy-element', ExpozyElement);
-  
+}
+
+// Регистриране на Web Component-а като expozy-element
+customElements.define('expozy-element', ExpozyElement);
