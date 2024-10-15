aaf5d03aa771bb957158581f7755f9ee7a2fde6e
DeucalionCyberSecurity <admin@d3vsoft.com>
2024-10-15 20:24:29 +0300

    Update expozy.js

--- expozy.js
+++ expozy.js
@@ -10,6 +10,15 @@ class ExpozyElement extends HTMLElement {
         const urlParams = new URL(scriptSrc).searchParams;
         const projectName = urlParams.get('p');
 
+        // Създаване на стил за бял фон
+        const loadingStyle = document.createElement('style');
+        loadingStyle.innerHTML = `
+            body {
+                visibility: hidden!important; /* Скрива съдържанието, докато не се заредят файловете */
+            }
+        `;
+        document.head.appendChild(loadingStyle);
+
         if (projectName) {
             Promise.all([
                 fetch(`${baseUrl}/${projectName}/js/import.json`).then(response => response.json()),
@@ -19,20 +28,28 @@ class ExpozyElement extends HTMLElement {
                     const link = document.createElement('link');
                     link.rel = 'stylesheet';
                     link.href = `${baseUrl}/${projectName}/css/${cssFile}`;
-                    document.head.appendChild(link); // Добавяме в <head>, за да влияе на глобалните стилове
+                    document.head.appendChild(link);
                 });
 
                 jsFiles.forEach(jsFile => {
                     const script = document.createElement('script');
                     script.src = `${baseUrl}/${projectName}/js/${jsFile}`;
                     script.defer = true;
-                    document.body.appendChild(script); // Добавяме скриптовете в <body>
+                    document.body.appendChild(script);
                 });
+
+                // Премахване на временния стил след зареждане на файловете
+                loadingStyle.remove();
+                document.body.style.visibility = 'visible';
             }).catch(error => {
-                // console.error('Грешка при зареждане на файловете:', error);
+                console.error('Грешка при зареждане на файловете:', error);
+                loadingStyle.remove();
+                document.body.style.visibility = 'visible';
             });
         } else {
-            // console.warn("Не е зададен проектен параметър 'p' в URL адреса на скрипта.");
+            console.warn("Не е зададен проектен параметър 'p' в URL адреса на скрипта.");
+            loadingStyle.remove();
+            document.body.style.visibility = 'visible';
         }
     }
 }
